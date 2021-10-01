7fdb0eb1507bb4c14e62ff4eef4322256bafbb96
Rohansr2727 <89068391+Rohansr2727@users.noreply.github.com>
2021-10-01 09:44:13 +0530

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def sum(numberes):
+    total =0
+    for x in numberes:
+        total+= x
+    
+        return total
+    print(sum(8,2,3,0,7))
+
+
+# In[5]:
+
+
+def reverse(s):
+    str =' '
+    
+    
+    for i in s:
+        str = i+str
+        return str
+    
+    
+    print(reverse( "dcba4321"))
+    
+
+
+# In[18]:
+
+
+def string_test(s):
+
+    d ={"UPPER_CASE":0,"LOWERCASE":0}
+    if c in s:
+         d["LOWER_CASE"]+=1
+    else:
+        
+          print("no of upper case charecteres ", d["UPPER_CASE"])
+          
+          PRINT("NO OF LOWER CASE CHARECTERES",D["LOWER CASE"])
+    
+    
+    
+    print(string_test('The quick Brow Fox'))
+        
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
