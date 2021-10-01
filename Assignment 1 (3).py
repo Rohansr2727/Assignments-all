d932cee7a2d6d97a6dff7260dc069d9ad8511d4a
Rohansr2727 <89068391+Rohansr2727@users.noreply.github.com>
2021-10-01 09:43:53 +0530

    Add files via upload

---
+++ Assignment 1 (3).py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+
+
+
+# In[13]:
+
+
+x= 'EDYODA'
+print(x[::-1])
+
+
+# In[12]:
+
+
+
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+count_even = 0
+count_odd = 0
+for x in numbers:
+    if x % 2:
+        count_even += 1
+else:
+
+        count_odd += 1
+print("number of even numberes:",count_even)
+print("number of odd numberes",count_odd)
+
+
+# In[ ]:
+
+
+
+
